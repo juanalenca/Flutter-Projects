e9acd96f53e90883c7b2a2dc41359ea8c5b10499
Juan Alencar de Barros <127232363+juanalenca@users.noreply.github.com>
2024-09-15 19:17:12 -0300

    Update main.dart

--- devel/stack/lib/main.dart
+++ devel/stack/lib/main.dart
@@ -4,50 +4,58 @@ void main() {
   runApp(const MyApp());
 }
 
-
+// A classe MyApp é um StatelessWidget que define a estrutura principal da aplicação.
 class MyApp extends StatelessWidget {
 
+  // Construtor constante, garantindo que a instância seja imutável e otimizando o desempenho.
   const MyApp({super.key});
 
+  // O método build constrói a interface do usuário da aplicação.
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
-
+      // Define a página principal da aplicação usando um Scaffold, que fornece uma estrutura padrão
+      // como appBar, body e outros elementos.
       home: Scaffold(
 
+        // Barra de título (AppBar) da aplicação.
         appBar: AppBar(
-          title: const Text("Stack App"),
+          title: const Text("Stack App"), // Define o texto exibido no título.
         ),
 
-        //o stack sobrepoem elementos em outros elementos
+        // Stack permite sobrepor widgets uns sobre os outros na ordem definida.
         body: Stack(
 
-          //Alinhamento dos elementos
+          // Define o alinhamento padrão dos widgets dentro do Stack. 
+          // Aqui, todos os widgets são alinhados ao centro.
           alignment: AlignmentDirectional.center,
 
           children: [
 
+            // Primeiro Container verde que ocupa 500x500 pixels.
             Container(
               width: 500,
               height: 500,
-              color: Colors.green,
+              color: Colors.green, // Cor de fundo verde.
             ),
 
+            // Segundo Container amarelo, menor, que ocupa 300x300 pixels, sobreposto ao verde.
             Container(
               width: 300,
               height: 300,
-              color: Colors.yellow,
+              color: Colors.yellow, // Cor de fundo amarela.
             ),
 
-            //mudando o posicionamento do lemento
+            // Positioned permite modificar a posição de um widget específico dentro do Stack.
+            // Aqui, o Container azul é posicionado a 200 pixels da borda inferior e direita.
             Positioned(
               bottom: 200,
               right: 200,
               child: Container(
                 width: 100,
                 height: 100,
-                color: Colors.blue,
+                color: Colors.blue, // Cor de fundo azul.
               ),
             ),
 
@@ -58,7 +66,5 @@ class MyApp extends StatelessWidget {
       ),
 
     );
-
   }
-
 }
